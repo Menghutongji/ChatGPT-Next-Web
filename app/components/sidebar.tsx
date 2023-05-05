7c180b1510f7388050af179635829140fc9086e4
Menghutongji <128691427+Menghutongji@users.noreply.github.com>
2023-05-06 01:31:42 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Chat with you</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AI技术交流群：823087064--每天提供免费API
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
