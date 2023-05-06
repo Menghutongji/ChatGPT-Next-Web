d493fecc9b5b6a60dd0d82ae215852af6014d2f6
Menghutongji <128691427+Menghutongji@users.noreply.github.com>
2023-05-07 01:38:52 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>Chat with you</div>
         <div className={styles["sidebar-sub-title"]}>
-          Your assistant
+          AI技术交流群：823087064
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
